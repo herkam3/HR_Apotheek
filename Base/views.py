43dff0ce0c8050c9c612c48dde0e6fca83c69a2b
Herkam3 <1078244@hr.nl>
2024-06-14 00:24:33 +0200

    Use of Startapp base in django.

--- Base/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
-from django.contrib import messages
-from .forms import NameForm, DistanceForm
-from .models import Time, Distance
-
-def index(request):
-    return render(request, 'base/index.html')
