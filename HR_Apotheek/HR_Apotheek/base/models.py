b4d1a1d75dc096f47298eea65d5be7a1a67a3f85
BoraHR <1066706@hr.nl>
2024-06-14 00:01:28 +0200

    - noted models as comments

---
+++ HR_Apotheek/HR_Apotheek/base/models.py
@@ -0,0 +1,24 @@
+# Models
+
+
+# Medicine:
+# Name
+# Manufacturer
+# Cures
+# SideEffects
+
+
+# Collection:
+# Medicine (FK)
+# User (FK)
+# Date
+# Collected
+# CollectedApproved
+# CollectedApprovedBy (FK)
+
+
+# Profile:
+# User (FK)
+# BioText
+# City
+# DateOfBirth
