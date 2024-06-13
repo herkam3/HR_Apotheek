4ac86dfa0a3d5178ac1088f793c5aa76304c1a81
Herkam3 <1078244@hr.nl>
2024-06-14 00:46:44 +0200

    further setup in views and urls.py

--- HR_Apotheek/base/admin.py
+++ HR_Apotheek/base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.urls import path, include
 
-# Register your models here.
+urlpattern = [
+    path('admin/' admin.site.urls),
+    path("base/", include("base.urls")),
+]
