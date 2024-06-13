12385059de7439e571e52c95234a0f8ee278e2e6
Herkam3 <1078244@hr.nl>
2024-06-14 01:59:42 +0200

    setup forms.py a.d.v models.py

--- HR_Apotheek/base/forms.py
+++ HR_Apotheek/base/forms.py
@@ -1,1 +1,18 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import Medicine, Collection, Profile
+
+class MedicineForm(forms.ModelForm):
+    class Meta:
+        model = Medicine
+        fields = ['name', 'manufacturer', 'cures', 'side_effects']
+
+class CollectionForm(forms.ModelForm):
+    class Meta:
+        model = Collection
+        fields = ['medicine', 'user', 'date', 'collected', 'collected_approved', 'collected_approved_by']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio_text', 'city', 'date_of_birth']
