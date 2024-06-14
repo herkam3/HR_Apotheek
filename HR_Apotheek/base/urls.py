6f0baa8621f5020a727331e78effced6f126970c
Herkam3 <1078244@hr.nl>
2024-06-14 02:34:55 +0200

    create urls.py and link them.

--- HR_Apotheek/base/urls.py
+++ HR_Apotheek/base/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, as_view(), name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('medicines/', views.medicine_list, name='medicine_list'),
+    path('collections/', views.collection_list, name='collection_list'),
 ]
